2aa8587e7f75735e52e0b9fc8860cc8dc7b3abaf
nicholasbaffert <nicholas.baffert@studenti.isissgobetti.it>
2026-04-09 13:42:06 +0200

    punto C,D e main

---
+++ baffert.py
@@ -0,0 +1,23 @@
+from biblioteca import libri,prestiti
+
+
+# libri di esempio
+l1 = libri.crea_libro("1984", "George Orwell", "Fantascienza", 1)
+l2 = libri.crea_libro("Dune", "Frank Herbert", "Fantascienza", 2)
+l3 = libri.crea_libro("Il piccolo principe", "Antoine de Saint-Exupéry", "Romanzo", 1)
+l4 = libri.crea_libro("Harry Potter", "J.K. Rowling", "Fantasy", 3)
+
+# utenti di esempio
+u1 = prestiti.crea_utente("Mario", "Rossi")
+u2 = prestiti.crea_utente("Laura", "Bianchi")
+u3 = prestiti.crea_utente("Carlo", "Verdi")
+
+# Nel `main()` del tuo file principale (`cognome.py`):
+# - Crea almeno 4 libri diversi.
+# - Stampa le informazioni di ogni libro.
+# Nel `main()`:
+# - Filtra i libri di un genere specifico e stampa i titoli.
+# - Stampa i titoli dei libri disponibili.
+# - Cerca i libri di un autore e stampa i titoli trovati.
+
+stampa_informazioni
--- biblioteca/prestiti.py
+++ biblioteca/prestiti.py
@@ -4,8 +4,22 @@ def crea_utente(nome: str, cognome: str) -> dict:
     return {"nome": nome, "cognome":cognome}
 
 def crea_biblioteca() -> dict:
-    return {}
+    return {"libri": []}
 
 def aggiungi_libro(biblioteca: dict, libro: dict) -> None:
-    
+    biblioteca['libri'].append(libro)
 
+def presta_libro(biblioteca: dict, libro:dict) -> bool:
+    if libro_disponibile(libro) == True:
+        biblioteca['libri'] -= 1
+        return True
+    return False
+
+def restituisci_libro(biblioteca: dict, libro: dict, prestiti: list) -> bool:
+    if presta_libro
+        biblioteca['libri'] += 1
+        return True
+
+def libri_prestati(biblioteca: dict) -> list[dict]:
+    prestati = [dict]
+    for 
